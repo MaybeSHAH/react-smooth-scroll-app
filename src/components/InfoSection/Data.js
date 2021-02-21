690a284f7bf725538ee8fa5cc198b1f2d36ad252
Maybe_shah <liyakatshaikh07@gmail.com>
2021-02-21 16:21:24 +0530

    Complete info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+import logo from '../../images/svg-1.svg';
+
+export const homeObjOne= {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Maybe_shah Gaming',
+    headLine: 'MLBB Matches will be host here...',
+    description: 'Giving common players the opportunity to show his skills (It tooks 2m to register)',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: logo,
+    alt: 'Car',
+    dark: true,
+    dark2: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo= {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Maybe_shah Gaming',
+    headLine: 'MLBB Matches will be host here...',
+    description: 'Giving common players the opportunity to show his skills (It tooks 2m to register)',
+    buttonLabel: 'Get Started',
+    imgStart: true,
+    img: logo,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree= {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Maybe_shah Gaming',
+    headLine: 'MLBB Matches will be host here...',
+    description: 'Giving common players the opportunity to show his skills (It tooks 2m to register)',
+    buttonLabel: 'Start now',
+    imgStart: false,
+    img: logo,
+    alt: 'Paper',
+    dark: false ,
+    primary: false,
+    darkText: true
+}
